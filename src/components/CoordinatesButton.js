ebd4a2cf71fed087b9c6fbce60d1830131af467b
Micah Banschick <micha.ban@gmail.com>
2021-03-23 16:36:47 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -5,4 +5,8 @@ export default class CoordinatesButton extends Component {
   render() {
     return <button onClick={ e => this.props.onReceiveCoordinates([e.clientX, e.clientY])} ></button>
   }
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> 39c600ded9dcf0f4faf67aa9b8dea7572c6631ad
